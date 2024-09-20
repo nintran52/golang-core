3bc5e75027fb70c3416d83be7e3cd2e75d85bc32
Nin Tran <nin.tran52@gmail.com>
2024-09-20 23:14:15 +0700

    Array

--- main.go
+++ main.go
@@ -5,4 +5,7 @@ import "fmt"
 func main() {
 	name := "Golang Learners"
 	fmt.Printf("Welcome, %s, to the Todo List App!\n", name)
+
+	todos := []string{"Buy groceries", "Complete homework", "Read Golang docs"}
+	fmt.Println(todos)
 }
