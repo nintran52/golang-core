5f79ecfdc6925c94a4535ae215e1dcd8255de3ce
Nin Tran <nin.tran52@gmail.com>
2024-09-20 23:28:32 +0700

    Concurrent

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Todo struct {
 	Task      string
@@ -24,6 +27,16 @@ func main() {
 	} else {
 		fmt.Println("You have tasks to do!")
 	}
+
+	tasks := make(chan string)
+
+	go addTask("Buy groceries", tasks)
+	go addTask("Complete homework", tasks)
+
+	for i := 0; i < 2; i++ {
+		task := <-tasks
+		fmt.Printf("Task added: %s\n", task)
+	}
 }
 
 func printTodos(todos []Todo) {
@@ -34,4 +47,9 @@ func printTodos(todos []Todo) {
 		}
 		fmt.Printf("%s: %s\n", todo.Task, status)
 	}
+}
+
+func addTask(task string, c chan string) {
+	time.Sleep(2 * time.Second) // Simulate adding tasks that take time
+	c <- task
 }
