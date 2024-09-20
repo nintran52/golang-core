1eeb1dcaca32dd5b5c4fd3543138b0f11efbb537
Nin Tran <nin.tran52@gmail.com>
2024-09-20 23:50:14 +0700

    Package

--- main.go
+++ main.go
@@ -1,42 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
-type Todo struct {
-	Task      string
-	Completed bool
-}
-
-type TodoList struct {
-	todos []Todo
-}
-
-type TaskManager interface {
-	Add(task string)
-	Complete(task string)
-}
-
-func (t *TodoList) Add(task string) {
-	todo := Todo{Task: task, Completed: false}
-	t.todos = append(t.todos, todo)
-	fmt.Printf("Added: %s\n", task)
-}
-
-func (t *TodoList) Complete(task string) {
-	for i := range t.todos {
-		if t.todos[i].Task == task {
-			t.todos[i].Completed = true
-			fmt.Printf("Completed: %s\n", task)
-			return
-		}
-	}
-	fmt.Printf("Task not found: %s\n", task)
-}
+import "todolist/todolist"
 
 func main() {
-	var manager TaskManager = &TodoList{}
+	var manager todolist.TaskManager = &todolist.TodoList{}
 
 	manager.Add("Buy groceries")
 	manager.Add("Complete homework")
---
+++ todolist/todolist.go
@@ -0,0 +1,34 @@
+package todolist
+
+import "fmt"
+
+type Todo struct {
+	Task      string
+	Completed bool
+}
+
+type TodoList struct {
+	todos []Todo
+}
+
+type TaskManager interface {
+	Add(task string)
+	Complete(task string)
+}
+
+func (t *TodoList) Add(task string) {
+	todo := Todo{Task: task, Completed: false}
+	t.todos = append(t.todos, todo)
+	fmt.Printf("Added: %s\n", task)
+}
+
+func (t *TodoList) Complete(task string) {
+	for i := range t.todos {
+		if t.todos[i].Task == task {
+			t.todos[i].Completed = true
+			fmt.Printf("Completed: %s\n", task)
+			return
+		}
+	}
+	fmt.Printf("Task not found: %s\n", task)
+}
